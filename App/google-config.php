bc71d8057ffc19e000a112ef6b85b2b83531974e
omarmaged23 <omarmaged292@gmail.com>
2025-09-18 00:35:50 +0300

    clean app

--- App/google-config.php
+++ App/google-config.php
@@ -1,4 +1,4 @@
 <?php
-const GOOGLE_CLIENT_ID = '280805324061-s2hfvrhg5u0eco5f2fjvporp4hb12s54.apps.googleusercontent.com';
-const GOOGLE_CLIENT_SECRET = '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';
-const GOOGLE_REDIRECT_URI = 'http://localhost/native/google-auth/google-callback.php';
+const GOOGLE_CLIENT_ID = '';
+const GOOGLE_CLIENT_SECRET = '';
+const GOOGLE_REDIRECT_URI = '';
